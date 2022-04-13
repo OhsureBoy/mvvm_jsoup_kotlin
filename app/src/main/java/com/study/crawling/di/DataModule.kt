35005fa847ce2de4b06d6bca95de3912336e43dd
Zisouu <47763201+Zisouu@users.noreply.github.com>
2022-04-13 09:58:33 +0900

    add : hilt Data Module

---
+++ app/src/main/java/com/study/crawling/di/DataModule.kt
@@ -0,0 +1,17 @@
+package com.study.crawling.di
+
+import com.study.crawling.data.DataRepository
+import com.study.crawling.data.DataRepositorySource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataModule {
+    @Binds
+    @Singleton
+    abstract fun provideDataRepository(dataRepository: DataRepository): DataRepositorySource
+}
