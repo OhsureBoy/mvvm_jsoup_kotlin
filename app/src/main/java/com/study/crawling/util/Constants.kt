cc2e51f7fa08808710df8b707d0d7d282523ef7a
Zisouu <47763201+Zisouu@users.noreply.github.com>
2022-04-13 09:58:58 +0900

    add : Basic Constants

---
+++ app/src/main/java/com/study/crawling/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.study.crawling.util
+
+const val SPLASH_DELAY = 3000
+const val USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36"
+const val CRAWLING_URL = "https://www.gettyimages.com/photos/collaboration?assettype=image&sort=mostpopular&phrase=collaboration&license=rf,rm&page="
